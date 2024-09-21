2dfeda1de3aa41da80454a7b8aff749bc68c6340
saikiran <saikiran.tokala@digitap.ai>
2024-09-21 11:27:45 +0530

    initial commit

---
+++ DTVKYC.xcframework/ios-arm64/DTVKYC.framework/Headers/DTVKYC.h
@@ -0,0 +1,18 @@
+//
+//  DTVKYC.h
+//  DTVKYC
+//
+//  Created by Sai Kiran on 19/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DTVKYC.
+FOUNDATION_EXPORT double DTVKYCVersionNumber;
+
+//! Project version string for DTVKYC.
+FOUNDATION_EXPORT const unsigned char DTVKYCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTVKYC/PublicHeader.h>
+
+
